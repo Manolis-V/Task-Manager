597a5d92d9361966e5519a173f2e7493f263f201
Manolis Voutetakis <manolisvoutetakis@gmail.com>
2024-10-16 14:16:38 +0300

    Basic CRUD Functionality and APIs Added

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Task
+from .serializers import TaskSerializer
+from django.http import JsonResponse
+from django.http import HttpResponse
+
+class TaskListCreate(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# Homepage view for '/'
+def homepage(request):
+    return HttpResponse("<h1>Welcome to the Task Manager API</h1>")
